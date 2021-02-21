004ddc1b6d2cc13cc0b51cfe02214adfc5bddfce
Mihail Marian <mihailmarian12@gmail.com>
2021-02-21 10:52:04 +0000

    private tasks

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -6,7 +6,9 @@ export const Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isServer) {
   Meteor.publish("tasks", function tasksPublication() {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [{ private: { $ne: true } }, { owner: this.userId }],
+    });
   });
 }
 
@@ -35,5 +37,17 @@ Meteor.methods({
     check(setChecked, Boolean);
 
     Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+  "tasks.setPrivate"(taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+
+    const task = Tasks.findOne(taskId);
+
+    if (task.owner !== this.userId) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
   },
 });
